4ca69560bd8d428928abb52f407f147b47ae30fc
stevenmcfarlane <stevemac72@gmail.com>
2016-03-03 11:00:21 -0800

    set the configuration for the USCD project this has not been pushed to deployment yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,10 +2,10 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 # Admins
-admins = [{ email: 'brey@iit.edu', first_name: 'Eric', last_name: 'Brey', password: 'REUappIIT'},
-          { email: 'damico@iit.edu', first_name: 'Cathie', last_name: "D'amico", password: 'REUappIIT' },
-          { email: 'weaver@iit.edu', first_name: 'Renee', last_name: 'Weaver', password: 'REUappIIT' },
-          { email: 'rob@notch8.com', first_name: 'Rob', last_name: 'Kaufman', password: 'REUappIIT' }]
+admins = [{ email: 'aengler@ucsd.edu', first_name: 'Adma', last_name: 'Engler', password: 'REUappUCSD'},
+          { email: 'rgaetani@eng.ucsd.edu', first_name: 'Roberto', last_name: "Gaetani", password: 'REUappUCSD' },
+          { email: 'crystal@notch8.com', first_name: 'Crystal', last_name: 'Richardson', password: 'REUappUCSD' },
+          { email: 'rob@notch8.com', first_name: 'Rob', last_name: 'Kaufman', password: 'REUappUCSD' }]
 
 admins.map { |user| admin = User.new(user); admin.confirmed_at = DateTime.now; admin.save; }
 
