14eb03af66a160e1829f258e39bf4336d5da08cd
Rob Kaufman <rob@notch8.com>
2016-03-04 15:15:34 -0800

    more rqi conversions

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,5 +67,5 @@ Reuman::Application.configure do
   config.action_mailer.smtp_settings = { :enable_starttls_auto => false, :host => 'localhost' }
   Rails.application.routes.default_url_options = { :protocol => 'https',  :host => 'reumanager.com/ucsd-eng' }
 
-  config.action_controller.relative_url_root = '/rqi'
+  config.action_controller.relative_url_root = '/ucsd-eng'
 end
