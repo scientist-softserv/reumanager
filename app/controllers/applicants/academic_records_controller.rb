362fcb1f95aa4e492f5aa773e20ecc694a70d29a
stevenmcfarlane <stevemac72@gmail.com>
2016-12-29 16:35:37 -0800

    don't add award by default

--- app/controllers/applicants/academic_records_controller.rb
+++ app/controllers/applicants/academic_records_controller.rb
@@ -4,7 +4,7 @@ class Applicants::AcademicRecordsController < ApplicationController
 
   def edit
     current_applicant.records.build unless current_applicant.records.count > 0
-    current_applicant.awards.build unless current_applicant.awards.count > 0
+    # current_applicant.awards.build unless current_applicant.awards.count > 0
 
     render :edit
   end
