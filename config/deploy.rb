c2bde22631a69e5367ceeddd250e7a5a76013296
Steven McFarlane <stevemac72@gmail.com>
2018-10-26 14:58:42 -0700

    basic env and deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ require "rvm/capistrano"
 #set :whenever_command, "bundle exec whenever"
 #require "whenever/capistrano"
 
-set :application, "glade" #matches names used in smf_template.erb
+set :application, "seti" #matches names used in smf_template.erb
 set :repository,  "https://github.com/notch8/reumanager.git"
 set :branch, "#{application}"
 set :domain, 'indra'
