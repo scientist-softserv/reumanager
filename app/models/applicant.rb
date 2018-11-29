d6cb4802cb3606821aefd89c39dbab871d648726
kellyChex <kelly@notch8.com>
2018-11-28 21:56:13 -0800

    Move fields from applicant to academic records to
    make validations in more appropriate area

--- app/models/applicant.rb
+++ app/models/applicant.rb
@@ -14,7 +14,7 @@ class Applicant < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :recoverable, :rememberable,
          :trackable, :validatable, :lockable, :timeoutable, :confirmable
-  attr_accessible :academic_level, :email, :green_card_holder, :password, :password_confirmation, :confirmed_at,
+  attr_accessible :academic_level, :email, :green_card_holder, :password, :password_confirmation,
                   :remember_me, :first_name, :last_name, :phone, :dob, :citizenship, :disability,
                   :gender, :ethnicity, :race, :gpa_comment, :lab_skills, :addresses_attributes,
                   :awards_attributes, :records_attributes, :recommendations_attributes, :recommenders_attributes,
