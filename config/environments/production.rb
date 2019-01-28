dca4c5daf8d56b59e56a684ed50d8acf62899ef0
jpengstrom <jpengst@gmail.com>
2019-01-28 15:44:48 -0800

    fixed mailer url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Reuman::Application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
 
   # See everything in the log (default is :info)
   # config.log_level = :debug
@@ -63,7 +63,7 @@ Reuman::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :protocol => 'https',  :host => 'reumanager.com/seti' }
+  config.action_mailer.default_url_options = { :protocol => 'https',  :host => 'https://reumanager.com/seti' }
   config.action_mailer.smtp_settings = { :enable_starttls_auto => false, :host => 'localhost' }
   Rails.application.routes.default_url_options = { :protocol => 'https',  :host => 'reumanager.com/seti' }
 
