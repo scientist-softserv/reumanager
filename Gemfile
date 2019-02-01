85eeaa54e1ec106acd20c0341c9a1bba7dec20b9
Steven McFarlane <stevemac72@gmail.com>
2019-02-01 14:47:39 -0700

    try to get print.css to compile

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
-source 'http://gems.github.com'
 source 'https://rubygems.org'
 
 gem 'rails'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,11 @@ Reuman::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += ['rails_admin/rails_admin.css', 'rails_admin/rails_admin.js', 'print.scss']
+  config.assets.precompile += [
+    'rails_admin/rails_admin.css',
+    'rails_admin/rails_admin.js',
+    'print.css'
+  ]
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
