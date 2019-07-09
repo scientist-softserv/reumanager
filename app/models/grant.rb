0b4b716eb575cb553cbe4b70fe1974ea68611e20
Aaron <prenger11@gmail.com>
2019-07-09 13:58:34 -0700

    fixed sources typo

--- app/models/grant.rb
+++ app/models/grant.rb
@@ -31,6 +31,6 @@ class Grant < ActiveRecord::Base
     Snippet.create(name: 'Program Highlights', description: '', grant_id: self.id)
     Snippet.create(name: 'Eligibility Requirements', description: '', grant_id: self.id)
     Snippet.create(name: 'Application Information', description: '', grant_id: self.id)
-    Snippet.create(name: 'Acknowledgment Of Funding Soruces', description: 'Please provide text to be included on the site as acknowledgment of program funding. In addition (or instead) you may send an image of the funding agency logo.', grant_id: self.id)
+    Snippet.create(name: 'Acknowledgment Of Funding Sources', description: 'Please provide text to be included on the site as acknowledgment of program funding. In addition (or instead) you may send an image of the funding agency logo.', grant_id: self.id)
   end
 end
