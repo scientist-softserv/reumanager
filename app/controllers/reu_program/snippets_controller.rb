598308b82e2e23924979542fce1ea17e3b3c36bf
Ubuntu <ubuntu@ip-172-31-8-255.us-west-2.compute.internal>
2019-07-10 23:11:35 +0000

    WIP fixing SnippetsController before finishing edit views

--- app/controllers/reu_program/snippets_controller.rb
+++ app/controllers/reu_program/snippets_controller.rb
@@ -15,12 +15,24 @@ module ReuProgram
     end
 
     def edit
+      @snippet = current_program_admin.snippets.find(params[:id])
     end
 
     def update
+      if @snippet.update(snippet_params)
+        redirect_to @snippet, notice: 'Snippet was successfully updated.'
+      else
+        render :edit
+      end
     end
 
     def destroy
+    end
+    
+    private
+    
+    def snippet_params
+      params.require(:snippet).permit(:name, :description, :value, :grant_id)
     end
   end
 end
