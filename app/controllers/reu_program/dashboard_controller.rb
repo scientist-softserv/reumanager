3c1fb50cd4dd53a6b243b13856d14e5d57db4f78
Amanda MacGregor <amanda.macgregor13@gmail.com>
2019-09-20 15:24:10 -0700

    quick adjustment to dashboard controller

--- app/controllers/reu_program/dashboard_controller.rb
+++ app/controllers/reu_program/dashboard_controller.rb
@@ -3,7 +3,7 @@ module ReuProgram
     def index
 
 
-      @applicants = Role.where(name: "Applicant").all
+      @applicants = Application.all
       
       @sum = Application.count
       @count_started = Application.where(state: "Started").count
