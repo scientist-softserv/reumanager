ad0c8cd484d1fb1ca0a868a30f8ccffa6bf05206
Charlie Montejo <cmontejo@protonmail.com>
2020-03-16 21:06:14 +0000

    Checkbox

--- app/javascript/components/fields/checkbox_field.js
+++ app/javascript/components/fields/checkbox_field.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
-export default function Checkbox ({label,value,onChange,props}) {
+export default function Checkbox ({label,value,onChange}) {
   const handleChange = (e) => {
-    var value = e.target.value
-    onChange(value)
-    props(value)
+    console.log(value)
+    onChange(e.target.checked)
   }
   return (
     <div className = 'form-group'>
         <label> {label} </label>
       <input 
-      type = {'checkbox'}
-      onChange = {handleChange} 
-      value = {value}
-      props = {false}
+        type = 'checkbox'
+        onChange = {handleChange} 
+        checked = {value}
       />
     </div>
   )
