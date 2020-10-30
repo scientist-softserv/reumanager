369fe1fbfc094f14b1b5e4d9d1cf6a0ac1a4fed0
Steven McFarlane <stevemac72@gmail.com>
2020-10-29 20:49:10 -0600

    add validations to invoice model

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,3 +1,8 @@
 class Invoice < ApplicationRecord
   belongs_to :grant
+
+  validates :university_or_institution, :department, :program_title,
+            :contact_name, :email, :phone, :billing_name, :billing_address,
+            :billing_email, :billing_phone, :po_number,
+            presence: true
 end
