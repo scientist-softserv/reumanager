bb8bcbfbcedc7197bf34af6bb30af52b19010496
Steven McFarlane <stevemac72@gmail.com>
2020-11-25 14:49:22 -0700

    test for deploy

--- ops/entrypoint.sh
+++ ops/entrypoint.sh
@@ -5,7 +5,8 @@ set -e
 rm -f /app/tmp/pids/server.pid
 
 yarn install
-bundle check || bundle install
+bundle check
+bundle install
 ./bin/webpack
 echo "Starting $@"
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
