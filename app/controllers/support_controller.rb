9bb8ba85fffea785e2e0019c51c87834d2afd75a
summer-cook <summercook@bellsouth.net>
2021-02-26 09:28:16 -0800

    This commit adds asterisks to required form fields & makes the phone field in the support_controller permitted but not required

--- app/controllers/support_controller.rb
+++ app/controllers/support_controller.rb
@@ -1,17 +1,5 @@
 class SupportController < ApplicationController
 
-    # def send_support_email
-    #    @support_email = SupportMailer.support_email(params[:subject], params[:first_name], params[:last_name], params[:email], params[:phone], params[:program_name], params[:message])
-    #    @support_email.deliver
-    #     if @support_email[:subject].blank?
-    #         redirect_to support_path
-    #         flash[:alert] = "Please fill in all required fields"
-    #     else
-    #         redirect_to support_path
-    #         flash[:notice] = "Message Sent"
-    #   end
-    # end
-
     def send_support_email
         
         @support_email = SupportMailer.support_email(params[:subject], params[:first_name], params[:last_name], params[:email], params[:phone], params[:program_name], params[:message])
@@ -26,12 +14,8 @@ class SupportController < ApplicationController
         end
     end
 
-    # def cant_send_email?
-    #     @support_email[:subject].blank?
-    # end
-
 private
     def send_support_email_params
-        params.require(:subject, :first_name, :last_name, :email, :phone, :message).permit(:program_name)
+        params.require(:subject, :first_name, :last_name, :email, :message).permit(:phone, :program_name)
     end
   end
